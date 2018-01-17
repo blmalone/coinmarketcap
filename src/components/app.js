1ffdb53440dd2209e503394646d6635813ba3579
BlaineMalone <bmalone05@qub.ac.uk>
2018-01-17 21:48:59 +0000

    Added coin_list containers that will display the coin that has been searched. This is in the format of a <table>

--- src/components/app.js
+++ src/components/app.js
@@ -1,11 +1,13 @@
 import React, { Component } from 'react';
 import SearchBar from '../containers/search_bar';
+import CoinList from '../containers/coin_list';
 
 export default class App extends Component {
   render() {
     return (
       <div>
       	<SearchBar />
+      	<CoinList />
       </div>
     );
   }
---
+++ src/containers/coin_list.js
@@ -0,0 +1,17 @@
+import React, { Component } from 'react';
+
+export default class CoinList extends Component {
+	render() {
+		return (
+			<table className="table table-hover">
+				<thead>
+					<tr>
+						<th>Coin Name</th>
+					</tr>
+				</thead>
+				<tbody>
+				</tbody>
+			</table>
+		);
+	}
+}
