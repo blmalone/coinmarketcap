33f3f901384a9d8ce01d359a429e49865c582308
BlaineMalone <bmalone05@qub.ac.uk>
2018-01-17 20:09:29 +0000

    switch statement added to ensure that the reducer updates the coin state when it receives an action from FETCH_COIN action creator.

--- src/reducers/reducer_coin.js
+++ src/reducers/reducer_coin.js
@@ -1,5 +1,13 @@
+import { FETCH_WEATHER } from '../actions/index';
+
 //'state' concerns the state that this particular reducer is responsible for.
-export default function(state = null, action) {
-	console.log('Action Received: ', action);
+export default function(state = [], action) {
+	switch(action.type) {
+		case FETCH_WEATHER: //handle the payload when you're in the case statement.
+			//never state.push() -- concat doesn't mutate existing state, it returns new state.
+			return state.concat([ action.payload.data ]); 
+			//return [ action.payload.data, ...state ]; <-- this is identical to the above, it is es6 syntax. (this is reverse insert, easily changed by swap)
+
+	}
 	return state;
 }
