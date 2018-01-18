57411e978256dfb38e053eedbd78d14fcf2f8b8b
BlaineMalone <bmalone05@qub.ac.uk>
2018-01-18 15:37:09 +0000

    small change to comments at src/index.js

--- src/index.js
+++ src/index.js
@@ -7,7 +7,7 @@ import ReduxPromise from 'redux-promise';
 import App from './components/app';
 import reducers from './reducers';
 
-//added middleware to out application - can intercept and modify/block actions that are going to reducers.
+//added middleware to our application - can intercept and modify/block actions that are going to reducers.
 const createStoreWithMiddleware = applyMiddleware(ReduxPromise)(createStore);
 
 ReactDOM.render(
