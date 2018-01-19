d1643c273ceeb544072c7d4086794a2aae905a50
BlaineMalone <bmalone05@qub.ac.uk>
2018-01-19 16:54:51 +0000

    Exploring common functional programming concepts in js. Looking a high order functions like map,filter,reject,reduce. ToDo - look at more applications of the reduce function

---
+++ src/javascriptConceptPractice.js
@@ -0,0 +1,98 @@
+//Looking into Higer Order funcions - Fileter, Map, Reduce
+//################################################################################################################
+//To run this program just pass this js file as an arguement to node <arg> on the commandline. 
+var animals = [
+	{ name: 'fluffykins', species: 'rabbit'},
+	{ name: 'Caro', species: 'dog'},
+	{ name: 'Hamilton', species: 'dog'},
+	{ name: 'Harold', species: 'fish'},
+	{ name: 'Ursula', species: 'cat'},
+	{ name: 'Jimmy', species: 'fish'},
+];
+console.log('Getting only the dogs from animals array - using Filter');
+//Retrieve only the dogs from the animals array
+var dogs = [];
+for(var i = 0; i < animals.length; i++) {
+	if (animals[i].species === 'dog') {
+		dogs.push(animals[i]);
+	}
+}
+console.log(dogs);
+
+//Functional equivalent - much easier to read
+var isDog = function(animal) {
+	return animal.species === 'dog';
+}
+
+var dogs = animals.filter(isDog); //notice how the function is treated as a value and passed to the filter function.
+console.log(dogs);
+
+//################################################################################################################
+//Lets get all the names of the animals in an array.
+//Firstly with a normal for-loop
+console.log('Getting only the animal names - using Map');
+/*var animalNames = [];
+for(var i = 0; i<animals.length; i++) {
+	animalNames.push(animals[i].name); 
+}
+*/
+/*
+var animalNames = animals.map(function(animal) {
+	return animal.name;
+});
+*/
+//ES6 arrow functions - shorter and better
+//If your function logic fits on one line like this example, you can get ride of the return statement and the curly brackets.
+var animalNames = animals.map((x) => { return x.name; });
+console.log(animalNames);
+
+//################################################################################################################
+/*
+	What is Reduce?
+
+	Think of it as the multi-tool of list transformations. It can be used to express any list transformation.
+	You can fallback on this function if you can't find a prebuild list transformation like map, reject, filter. 
+*/
+var orders = [
+	{ amount: 250 },
+	{ amount: 400 },
+	{ amount: 100 },
+	{ amount: 325 }
+];
+
+/* 
+	In this case 'sum' is the object passed as the 2nd arg in reduce. 
+	The iterated item in orders is now the 2nd arg in callback function.
+*/
+var totalAmount = orders.reduce((sum, order) => sum + order.amount, 0);
+/* 
+	Unlike map and filter, reduce want a second argument in the form of an object.
+	You can think of this object (zero in our case), as the starting point for our sum. 
+	The updated sum param is passed back into the callback for every new item in the array. 
+*/
+
+/* 
+var totalAmount = 0;
+for(var i = 0; i < orders.length; i++) {
+	totalAmount += orders[i].amount;
+}
+*/
+console.log(totalAmount);
+
+//################################################################################################################
+/*
+	More advanced Reduce:
+
+	Exploring more about the reduce function.
+*/
+
+
+
+
+
+
+
+
+
+
+
--- src/reducers/reducer_coin.js
+++ src/reducers/reducer_coin.js
@@ -1,11 +1,15 @@
 import { FETCH_COIN } from '../actions/index';
 
+//Redux lets you compose reducers rather than deal with the entire app state
+// inside each reducer. The result is that you don’t have to create a deep clone
+// of the entire app state every time you want to update just a small part of it.
 //'state' concerns the state that this particular reducer is responsible for.
 export default function(state = [], action) {
 	switch(action.type) {
 		case FETCH_COIN: //handle the payload when you're in the case statement.
 			console.log('FETCH_COIN: ', action.payload.data);
-			//never state.push() -- concat doesn't mutate existing state, it returns new state.
+			//never state.push() -- concat doesn't mutate existing state, it returns new state. 
+			//you could end up mutating shared state and then the function is NOT pure.
 			return state.concat([ action.payload.data[0] ]); 
 			//return [ action.payload.data, ...state ]; <-- this is identical to the above, it is es6 syntax. (this is reverse insert, easily changed by swap)
 	}
